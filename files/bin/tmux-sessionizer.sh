dabfd3499837b684d705991f08559c213f3f820f
adnannajjar <adnan.najjar1@gmail.com>
2025-05-29 13:31:36 +0400

    Add bin folder for custom scripts

---
+++ files/bin/tmux-sessionizer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# replace a set dirs to zoxide
+selected=$(zoxide query -i)
+
+selected_name=$(basename "$selected" | tr . _ | cut -d_ -f1)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+tmux switch-client -t $selected_name
